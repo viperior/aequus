ecbce99b53fc648067c74b486e166cad0e2d0924
Johnathon Stone <viperior@gmail.com>
2022-06-23 18:16:11 +0000

    Create the calc module

---
+++ calc.py
@@ -0,0 +1,26 @@
+"""Recipe calculator module"""
+
+class Item:
+    """This is a class to represent items that exist in a stable form, whether it can be made or
+    must be gathered from the natural world.
+
+    Attributes:
+    name (str): The name of the item
+    source (str): The source of the item, to allow distinguishing between items from different
+        sources
+    """
+
+    def __init__(self, name: str, source: str="Unknown"):
+        self.name = name
+        self.source = source
+
+    def info(self) -> str:
+        """Return all of the information about this item."""
+        return f"Item name: {self.name}\nItem source: {self.source}"
+
+def main() -> None:
+    """The main function to execute when the calc module is called"""
+    print("Hello world! I'm calc. Nice to meet you.")
+
+if __name__ == "__main__":
+    main()
