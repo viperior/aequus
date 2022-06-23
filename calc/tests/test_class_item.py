65ce3204a52ac96d35631de75e70f7e001dc88c6
Johnathon Stone <viperior@gmail.com>
2022-06-23 21:15:09 +0000

    Add expected 2nd blank line (linting error)

--- calc/tests/test_class_item.py
+++ calc/tests/test_class_item.py
@@ -8,6 +8,7 @@ def test_item_creation() -> None:
     stick_item = calc.Item(name="Stick", source="Minecraft")
     assert stick_item.name_with_source() == "Stick (Minecraft)"
 
+
 def test_item_info() -> None:
     """Test the info method of an Item object"""
     stick_item = calc.Item(name="Stick", source="Minecraft")
