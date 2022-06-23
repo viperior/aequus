db706783c4145d62ead5a16f815d0b9d9d49f4ca
Johnathon Stone <viperior@gmail.com>
2022-06-23 20:32:04 +0000

    Create a test for the calc module

---
+++ calc/tests/test_class_item.py
@@ -0,0 +1,8 @@
+"""Tests for the Item class"""
+
+import calc
+
+def test_item_creation() -> None:
+    """Test the creation of an Item object"""
+    stick_item = calc.Item(name="Stick", source="Minecraft")
+    assert stick_item.name_with_source() == "Stick (Minecraft)"
