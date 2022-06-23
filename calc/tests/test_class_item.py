a304aefd9b182b46ad9bece5bf38f0723d40fa55
Johnathon Stone <viperior@gmail.com>
2022-06-23 21:12:01 +0000

    Add a test for the Item.info() method

--- calc/tests/test_class_item.py
+++ calc/tests/test_class_item.py
@@ -7,3 +7,10 @@ def test_item_creation() -> None:
     """Test the creation of an Item object"""
     stick_item = calc.Item(name="Stick", source="Minecraft")
     assert stick_item.name_with_source() == "Stick (Minecraft)"
+
+def test_item_info() -> None:
+    """Test the info method of an Item object"""
+    stick_item = calc.Item(name="Stick", source="Minecraft")
+    stick_info = stick_item.info()
+    assert "Item name: Stick" in stick_info
+    assert "Item source: Minecraft" in stick_info
