c700f7d6b6497bff94fd7f101b955f7d5eae6c68
Johnathon Stone <viperior@gmail.com>
2022-06-23 21:41:05 +0000

    Create the RecipeComponent, Product, and Reactant classes

--- calc/__init__.py
+++ calc/__init__.py
@@ -22,3 +22,40 @@ class Item:
     def name_with_source(self) -> str:
         """Return the item name and the source as a string"""
         return f"{self.name} ({self.source})"
+
+
+class RecipeComponent:
+    """This is the parent class for Reactants and Products. It is the generic representation of a
+    combination of an Item and a quantity.
+
+    Attributes:
+    item (Item): The Item object
+    quantity (float): The quantity of the item. It has a float data type to allow precise
+        calculations and probabilistic quantities.
+    """
+
+    def __init__(self, item: Item, quantity: float):
+        self.item = item
+        self.quantity = quantity
+
+    def text(self) -> str:
+        """Return a string with the quantity and item name"""
+        return f"{self.quantity} {self.item.name}"
+
+
+class Product(RecipeComponent):
+    """This is the class for Products, which are recipe components created using one or more
+    reactants.
+    """
+    def info(self) -> str:
+        """Return the information about this product."""
+        return f"Product: {self.text()}"
+
+
+class Reactant(RecipeComponent):
+    """This is the class for Reactants, which are recipe components used to create one or more
+    products.
+    """
+    def info(self) -> str:
+        """Return the information about this reactant."""
+        return f"Reactant: {self.text()}"
