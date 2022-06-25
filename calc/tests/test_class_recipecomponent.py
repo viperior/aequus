a026ef20380f0dcdde830adfca30207f8526ab7b
Johnathon Stone <viperior@gmail.com>
2022-06-25 00:19:03 +0000

    Add a test case for the key and text methods of the RecipeComponent class

--- calc/tests/test_class_recipecomponent.py
+++ calc/tests/test_class_recipecomponent.py
@@ -15,3 +15,10 @@ def test_reactant_creation() -> None:
     plank_item = calc.Item(name="Oak Planks", source="Minecraft")
     plank_reactant = calc.Reactant(item=plank_item, quantity=2)
     assert plank_reactant.info() == "Reactant: 2 Oak Planks"
+
+
+def test_recipe_component(item_stick: calc.Item) -> None:
+    """Test the key() and text() methods of the RecipeComponent class"""
+    generic_component = calc.RecipeComponent(item=item_stick, quantity=4)
+    assert generic_component.key() == "4 Sticks (Minecraft)"
+    assert generic_component.text() == "4 Sticks"
