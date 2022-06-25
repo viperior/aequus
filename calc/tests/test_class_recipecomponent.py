dc063674023326238d792c446a59ec04c2c86466
Johnathon Stone <viperior@gmail.com>
2022-06-25 15:45:39 +0000

    Simplify test cases using fixtures

--- calc/tests/test_class_recipecomponent.py
+++ calc/tests/test_class_recipecomponent.py
@@ -3,17 +3,15 @@
 import calc
 
 
-def test_product_creation() -> None:
+def test_product_creation(item_stick: calc.Item) -> None:
     """Test the creation of a Product object"""
-    stick_item = calc.Item(name="Sticks", source="Minecraft")
-    stick_product = calc.Product(item=stick_item, quantity=4)
+    stick_product = calc.Product(item=item_stick, quantity=4)
     assert stick_product.info() == "Product: 4 Sticks"
 
 
-def test_reactant_creation() -> None:
+def test_reactant_creation(item_plank: calc.Item) -> None:
     """Test the creation of a Reactant object"""
-    plank_item = calc.Item(name="Oak Planks", source="Minecraft")
-    plank_reactant = calc.Reactant(item=plank_item, quantity=2)
+    plank_reactant = calc.Reactant(item=item_plank, quantity=2)
     assert plank_reactant.info() == "Reactant: 2 Oak Planks"
 
 
